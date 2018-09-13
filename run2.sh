379e10276df7300bbd952adf2b0ab0c3fb6864fc
Dmitry Rodin <madiedinro@gmail.com>
2018-09-13 17:28:41 +0300

    fix logs

--- run2.sh
+++ run2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
-echo "{_TEMP_STORE}/plugins: ${_TEMP_STORE}/plugins"
-echo $(ls -la ${_TEMP_STORE}/plugins)
-echo "{_PLUGINS}: ${_PLUGINS}"
+echo "-> {_TEMP_STORE}/plugins: ${_TEMP_STORE}/plugins"
+echo "->$(ls -la ${_TEMP_STORE}/plugins)"
+echo "->{_PLUGINS}: ${_PLUGINS}"
 ls -la $_PLUGINS
 
 if [ ! -d "$GF_PATHS_PLUGINS" ]; then
     mkdir "$GF_PATHS_PLUGINS"
 fi
 
-echo "{_PLUGINS}/2: ${_PLUGINS}"
+echo "-> {_PLUGINS}/2: ${_PLUGINS}"
 ls -la $_PLUGINS
 
+mv ${_TEMP_STORE}/dash ${_DATA}
+
 for f in $(ls -A ${_TEMP_STORE}/plugins); do
-    echo "${_TEMP_STORE}/plugins -> ${_PLUGINS}/$f"
+    echo "-> ${_TEMP_STORE}/plugins -> ${_PLUGINS}/$f"
     mv -u "${_TEMP_STORE}/plugins/$f" ${_PLUGINS}
 done
 
